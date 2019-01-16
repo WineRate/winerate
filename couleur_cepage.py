fde3c18d695f5af85296a81a63869f281c27f519
Fanny86 <fannynguyen86@gmail.com>
2019-01-16 14:39:09 +0100

    scrape couleur cepage bourgogne

--- couleur_cepage.py
+++ couleur_cepage.py
@@ -4,29 +4,55 @@ from bs4 import BeautifulSoup
 import html5lib
 
 
-f=str(open("/home/fitec/Bureau/links.txt","r").read())
-liste=f.split("\n")
+"""Resecement des liens"""
+lien = []
+url="https://www.vins-bourgogne.fr/nos-vins-nos-terroirs/la-bourgogne-et-ses-appellations/la-bourgogne-une-localisation-privilegiee,2377,9170.html?&args=Y29tcF9pZD0yMjA1JmFjdGlvbj12aWV3RnVsbExpc3RlQ2xpbWF0JmlkPSZ8"
+requete=requests.get(url)
+page_html=requete.content
+page_BS=BeautifulSoup(page_html, "html5lib")
 
-"""url="https://www.vins-bourgogne.fr/nos-vins-nos-terroirs/la-bourgogne-et-ses-appellations/aloxe-corton,2377,9170.html?&args=Y29tcF9pZD0yMjA1JmFjdGlvbj12aWV3RmljaGUmaWQ9MjExJnw%3D"
+count=len(page_BS.find_all('li',class_="{appellation_class}"))
+
+
+for i in range(count):
+    lien.append(page_BS.find_all('li',class_="{appellation_class}")[i].a.get("href"))
+
+
+"""transformer list en set pour eliminer les liens doublonnés"""
+lien_unique=set(lien)
+
+"""ajouter préfix http://www.vins-bourgogne.fr à tous les liens"""
+
+lien_corrige=[]
+prefix="https://www.vins-bourgogne.fr"
+
+lien_corrige=[prefix+i for i in lien_unique]
+
 """
+with open("lien_corrige", "w") as lien:
+    lien.write(str(lien_corrige))   """
+
+
 with open("couleur", "w") as cl:
     cl.write("")
     cl.close()
 
-for url in liste:
+for url in lien_corrige:
     requete=requests.get(url)
     page_html=requete.content
     page_BS=BeautifulSoup(page_html, "html5lib")
     couleur_text=page_BS.find_all('div', class_='portlet3')[0]
 
-    couleur1=str(couleur_text.find_all('div', class_='col-md-offset-1')[0]).split('<br/>')[1].split(" ")[1]
-
+    try:
+        couleur1=str(couleur_text.find_all('div', class_='col-md-offset-1')[0]).split('<br/>')[1].split(" ")[1]
+    except:
+        print("couleur1, page na pas ce contenu")
 
     try:
         cepage1=str(couleur_text.find_all('div', class_='col-md-offset-1')[0]).split('<br/>')[1].split(" ")[-1]
     except:
-        print (url)
-        print ("cepage1")
+        print("cepage2, page na pas ce contenu")
+
     try:
         couleur2=str(couleur_text.find_all('div', class_='col-md-offset-1')[0]).split('<br/>')[2].split(" ")[1]
     except:
@@ -55,4 +81,3 @@ for url in liste:
         cl.write(cepage1+'\n')
         cl.write(couleur2 + '\n')
         cl.write(cepage2+'\n')
-
